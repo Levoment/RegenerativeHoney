850cfd72f74b4129b2ca56f8736a2b42bbecc87d
Levoment <30224138+Levoment@users.noreply.github.com>
2022-04-13 01:11:46 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Regenerative Honey
+![Regenerative Honey](./src/main/resources/assets/regenerativehoney/icon.png)
+
+## About
+
+Minecraft mod that makes honey bottles give the most basic of regeneration potion effect to the player with the hunger and saturation of Chorus Fruit.
+
+## License
+
+The Unlicensed
---
+++ src/main/java/com/github/levoment/regenerativehoney/RegenerativeHoneyMod.java
@@ -0,0 +1,17 @@
+package com.github.levoment.regenerativehoney;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class RegenerativeHoneyMod implements ModInitializer {
+	// This logger is used to write text to the console and the log file.
+	// It is considered best practice to use your mod id as the logger's name.
+	// That way, it's clear which mod wrote info, warnings, and errors.
+	public static final Logger LOGGER = LoggerFactory.getLogger("regenerativehoney");
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
